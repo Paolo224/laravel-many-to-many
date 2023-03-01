d9ff889e5ae9c939b53e15902a4b332b1a93a59b
Paolo224 <picariellopaolo224@gmail.com>
2023-03-01 16:14:55 +0100

    inserimento della call TechnologySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypesTableSeeder::class,
+            TechnologiesSeeder::class,
             ProjectsTableSeeder::class,
         ]);
     }
