9be1543082c10a648d1f2245a869b34ae6f3adfc
Paolo224 <picariellopaolo224@gmail.com>
2023-03-01 17:37:48 +0100

    Aggiunta delle checkbox nel edit + contains

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -29,6 +29,15 @@
                 @endforeach
             </select>
         </div>
+        <div class="mb-3 d-flex justify-content-between">
+            @foreach ($technologies as $technology)
+            <div>
+                <input type="checkbox" class="form-check-input" name="technologies[]" value="{{$technology->id}}"
+                {{$project->technologies->contains($technology->id) ? 'checked' : ''}}>
+                <label class="form-check-label">{{$technology->name}}</label>
+            </div>                  
+            @endforeach
+        </div>
         <div class="mb-3">
             <label class="form-label">
                 Nome progetto
